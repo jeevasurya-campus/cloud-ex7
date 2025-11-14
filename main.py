a1f6b2bbfe0678d9e0dc0b0924cab4541595cd5f
JEEVASURYA P <jeevasurya.iotlab@gmail.com>
2025-11-14 09:37:47 +0000

    hi 2

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello")
-print("JY, HH, NML")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return '<h1>Hello from a PaaS Engine! JEEVASURYA :)</h1><p>My Python web application is live!</p>'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
